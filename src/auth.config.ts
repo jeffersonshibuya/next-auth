a39b9954e9ff720b308162e12908f5e7a1b71237
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2024-02-27 11:27:20 -0300

    Settings

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -20,14 +20,18 @@ export default {
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
 
-        if(validatedFields.success) {
-          const { email, password} = validatedFields.data
-          const user = await getUserByEmail(email)
-          if(!user || !user.password) return null
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+          
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
 
-          const passwordsMatch = await bcrypt.compare(password, user.password)
+          const passwordsMatch = await bcrypt.compare(
+            password,
+            user.password,
+          );
 
-          if(passwordsMatch) return user
+          if (passwordsMatch) return user;
         }
 
         return null;
--- src/auth.ts
+++ src/auth.ts
@@ -5,11 +5,13 @@ import { db } from "./lib/db"
 import { getUserById } from "./data/user"
 import { UserRole } from "@prisma/client"
 import { getTwoFactorConfirmationByUserId } from "./data/two-factor-confirmation"
+import { getAccountByUserId } from "./data/account"
 
 declare module "next-auth" {
   export interface User {
     role: UserRole,
     isTwoFactorEnabled: boolean;
+    isOAuth?: boolean;
   }
 }
 
@@ -55,6 +57,11 @@ export const {
       if(token.sub && session.user) session.user.id = token.sub
       if(token.role && session.user) session.user.role = token.role as UserRole
       if(session.user) session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean
+      if(session.user) { 
+        session.user.name = token.name
+        session.user.email = token.email as string
+        session.user.isOAuth = token.isOAuth as boolean
+      }
 
       return session;
     },
@@ -64,6 +71,11 @@ export const {
       const existingUser = await getUserById(token.sub)
       if(!existingUser) return token
 
+      const existingAccount = await getAccountByUserId(existingUser.id)
+
+      token.isOAuth = !!existingAccount
+      token.name = existingUser.name
+      token.email = existingUser.email
       token.role = existingUser.role
       token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled
       return token;
